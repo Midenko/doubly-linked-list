a24517017f085ee81e768ce7d56dc1e143b130b7
Midenko <kostyagrand@mail.ru>
2017-02-03 00:13:34 +0400

    Add files via upload

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,29 +1,79 @@
-const Node = require('./node');
-
 class LinkedList {
     constructor() {
-      var list = new Node;
+        this.firstNode = null;
+        this.lastNode = null;
+        this.count = 0;
     }
 
-    append(data) {}
+    append(data) {
+        var newNode = new Node(data = data);
+        if (!list.firstNode) {
+            list.firstNode = newNode;
+            list.lastNode = newNode;
+            list.count++;
+        }
+        else {
+            var node = new Node(this.data = data, this.prev = list.lastNode);
+            list.lastNode = node;
+            list.count++;
+        }
+    }
 
-    head() {}
+    head() {
+        if (!list.firstNode) {
+            return null;
+        }
+        return list.firstNode.data;	
+    }
 
-    tail() {}
+    tail() {
+        if (!list.firstNode) {
+            return null;
+        }
+        return list.lastNode.data;
+    }
 
-    at(index) {}
+    at(index) {
+        var tmp = list.lastNode;
+        for (var i = 0; i < list.count - index - 1; i++) {
+            tmp = tmp.prev;
+        }
 
-    insertAt(index, data) {}
+        return tmp.data;
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        var tmp = list.lastNode;
+        for (var i = 0; i < list.count - index - 1; i++) {
+            tmp = tmp.prev;
+        }
 
-    clear() {}
+        tmp.data = data;
+    }
 
-    deleteAt(index) {}
+    isEmpty() {
+        if (!list.firstNode) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    clear() {
+        list = new LinkedList;
+    }
+
+    deleteAt(index) {
+        var tmp = list.lastNode;
+        for (var i = 0; i < list.count - index - 1; i++) {
+            tmp = tmp.prev;
+        }
+        delete tmp;
+    }
 
     reverse() {}
 
     indexOf(data) {}
-}
 
-module.exports = LinkedList;
+}
